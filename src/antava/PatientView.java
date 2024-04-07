4701cf930a3487921e719b5ba3a4720490ce5133
Ruze <93176347+Ruze-alt@users.noreply.github.com>
2024-04-06 18:36:14 -0700

    fixed logo issue

--- src/antava/PatientView.java
+++ src/antava/PatientView.java
@@ -3,6 +3,7 @@ package antava;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
@@ -16,13 +17,16 @@ public class PatientView {
 		// TODO: this is a placeholder
 		VBox patientView = new VBox();
 		patientView.setBackground(new Background(new BackgroundFill(Color.rgb(201, 241, 253), CornerRadii.EMPTY, Insets.EMPTY)));
-    	patientView.setAlignment(Pos.CENTER);
-    	patientView.getChildren().add(Main.logo);
-    	patientView.getChildren().add(new Text("Successful login. Patient view here."));
+		patientView.setAlignment(Pos.CENTER);
+		
     	Scene patientScene = new Scene(patientView, 500, 750);
     	patientScene.setFill(Color.rgb(201, 241, 253));
     	
+    	ImageView newLogo = new ImageView(Main.logo.getImage());
+        Main.ImageDim(newLogo, patientView.getScene(), 0.8);
+        patientView.getChildren().add(newLogo);
+        patientView.getChildren().add(new Text("Successful login. Patient view here."));
+    	
     	return patientScene;
 	}
-
 }
