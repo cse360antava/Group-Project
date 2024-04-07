93999fca6833f1289ac0150ab14540037a3edf66
Ruze <93176347+Ruze-alt@users.noreply.github.com>
2024-04-06 18:34:44 -0700

    Fixed Logo issue

--- src/antava/Main.java
+++ src/antava/Main.java
@@ -3,6 +3,7 @@ package antava;
 import java.util.HashMap;
 
 import javafx.application.Application;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -27,11 +28,19 @@ public class Main extends Application {
     	
     	//TODO: Doctor view: on pressing the doctor view button, go to the doctor view
     	//TODO: Nurse view: on pressing the nurse view, go to the nurse view
-    	
+    	logo = new ImageView(new Image("file:src/antava/logo.jpg"));
         setScene(LoginView.getScene());
         primaryStage.show();
     }
     
+    public static void ImageDim(ImageView imageView, Scene scene, double ratio) {
+        double screenWidth = scene.getWidth();
+        double aspectRatio = imageView.getImage().getWidth() / imageView.getImage().getHeight();
+        double targetWidth = screenWidth * ratio;
+        double targetHeight = targetWidth / aspectRatio;
+        imageView.setFitWidth(targetWidth);
+        imageView.setFitHeight(targetHeight);
+    }
     public static void main(String[] args) {
     	launch(args);
     	
@@ -40,8 +49,7 @@ public class Main extends Application {
     	// TODO: This is a blatant hack and should be fixed later on
     	// I couldn't get it to read the image locally,
     	// so this fetches it from the GitHub organization's profile picture
-    	ImageView logo = new ImageView();
-    	logo.setImage(new Image("https://avatars.githubusercontent.com/u/163464474?s=400&u=863a0130fd7a0f9c4732e287690245896fae7e02&v=4"));
+    	//logo = new ImageView(new Image("https://avatars.githubusercontent.com/u/163464474?s=400&u=863a0130fd7a0f9c4732e287690245896fae7e02&v=4"));
     }
     
     
