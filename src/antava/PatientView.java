fea79c3b8bc665928555880170993e1c479aff4b
Ruze <93176347+Ruze-alt@users.noreply.github.com>
2024-04-09 05:23:12 -0700

    Example patient view file that has patient data storing

--- src/antava/PatientView.java
+++ src/antava/PatientView.java
@@ -3,6 +3,9 @@ package antava;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -30,14 +33,72 @@ public class PatientView {
     	ImageView newLogo = new ImageView(Main.logo.getImage());
     	newLogo.setFitWidth(250); 
     	newLogo.setFitHeight(250);
+    	
+        TextField firstNameField = new TextField();
+        TextField lastNameField = new TextField();
+        TextField birthDayField = new TextField();
+        TextField birthMonthField = new TextField();
+        TextField birthYearField = new TextField();
+        TextField ageField = new TextField();
+        TextArea healthIssueArea = new TextArea();
+        TextArea immuneHistoryArea = new TextArea();
+        TextArea currentPrescriptionsArea = new TextArea();
+        
+        firstNameField.setPromptText("First Name");
+        lastNameField.setPromptText("Last Name");
+        birthMonthField.setPromptText("Month");
+        birthDayField.setPromptText("Day");
+        birthYearField.setPromptText("Year");
+        ageField.setPromptText("Age");
+        healthIssueArea.setPromptText("Health");
+        immuneHistoryArea.setPromptText("Immune");
+        currentPrescriptionsArea.setPromptText("Medication");
+        
+        Button logoutButton = new Button("Log Out");
+        Button confirmButton = new Button("Confirm");
+        
+        logoutButton.setOnAction(event -> Main.setScene(LoginView.getScene()));
+        confirmButton.setOnAction(event -> {
+        	
+        	String firstName = firstNameField.getText();
+        	String lastName = lastNameField.getText();
+        	String birthDay = birthDayField.getText();
+        	String birthMonth = birthMonthField.getText();
+        	String birthYear = birthYearField.getText();
+        	String age = ageField.getText();
+            String healthIssues = healthIssueArea.getText();
+            String immuneHistory = immuneHistoryArea.getText();
+            String currentPrescriptions = currentPrescriptionsArea.getText();
+            
+            patient.patientData.editPatientData("firstName", firstName);
+            patient.patientData.editPatientData("lastName", lastName);
+            patient.patientData.editPatientData("birthDay", birthDay);
+            patient.patientData.editPatientData("birthMonth", birthMonth);
+            patient.patientData.editPatientData("birthYear", birthYear);
+            patient.patientData.editPatientData("age", age);
+            patient.patientData.editPatientData("previousHealthIssues", healthIssues);
+            patient.patientData.editPatientData("immunizationHistory", immuneHistory);
+            patient.patientData.editPatientData("currentPrescriptions", currentPrescriptions);
+            patient.createPatientID();
+  
+        	System.out.println("First Name: " + (String) patient.patientData.getDataRepo().get("firstName"));
+        	System.out.println("Last Name: " + (String) patient.patientData.getDataRepo().get("lastName"));
+        	System.out.println("Date Of Birth: " + (String) patient.patientData.getDataRepo().get("birthMonth") + "/" + 
+        			(String) patient.patientData.getDataRepo().get("birthDay") + "/" + 
+        			(String) patient.patientData.getDataRepo().get("birthYear")	
+        	);
+        	System.out.println("Age: " + (String) patient.patientData.getDataRepo().get("age"));
+        	System.out.println("Health Issues: " + (String) patient.patientData.getDataRepo().get("previousHealthIssues"));
+        	System.out.println("Immune History: " + (String) patient.patientData.getDataRepo().get("immunizationHistory"));
+        	System.out.println("Medications: " + (String) patient.patientData.getDataRepo().get("currentPrescriptions"));
+        	System.out.println("Patient ID: " + patient.getPatientID() + "\n");
+        });
         
         Text title = new Text("ANTAVA");
     	title.setFont(Font.font("Merriweather", FontWeight.BOLD, 20));
     	title.setStyle("-fx-fill: rgba(170, 103, 29, 0.8);");
-    	patientView.getChildren().add(newLogo);
-    	patientView.getChildren().add(title);
+    	patientView.getChildren().addAll(newLogo, title, firstNameField, lastNameField, birthDayField, birthMonthField, birthYearField, ageField, healthIssueArea, immuneHistoryArea, currentPrescriptionsArea, logoutButton, confirmButton);
     	
-        patientView.getChildren().add(new Text("Successful login. Patient view here."));
     	
     	return patientScene;
 	}
