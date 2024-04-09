a3049912565beb5158442606728d09ab7943f79a
Ruze <93176347+Ruze-alt@users.noreply.github.com>
2024-04-08 20:44:50 -0700

    Added any missing data inputs and removed any useless inputs.

--- src/antava/PatientDataRepository.java
+++ src/antava/PatientDataRepository.java
@@ -15,22 +15,21 @@ public class PatientDataRepository {
 		data.put("birthDay", 0);
 		data.put("birthMonth", 0);
 		data.put("birthYear", 0);
+		data.put("Age", 0);
 		data.put("phoneNumber", "0-000-000-0000");
 		data.put("email", "Enter email here");
 		data.put("insuranceCompany", "Enter insurance company here");
 		data.put("insurancePhoneNumber", "Enter insurance phone number here");
-		data.put("previousMedicalHistory", "Enter previous medical history here");
+		data.put("previousHealthIssues", "Enter previous health issues here");
+		data.put("immunizationHistory", "Enter immunization history here");
+		data.put("currentPrescriptions", "Enter current prescriptions here");
 		data.put("visitHistory", new ArrayList<Visit>());
 		data.put("bodyTemperature", 0);
-		data.put("pulseRate", 0);
-		data.put("respirationRate", 0);
 		data.put("bloodPressure", 0);
-		data.put("bloodOxygen", 0);
 		data.put("weight", 0);
-		data.put("bloodGlucoseLevel", 0);
+		data.put("height", 0);
 		data.put("allergies", "Enter allergies here");
 		data.put("healthConcerns", "Enter any health concerns here");
-		//TODO: implement physical exam reports in full, for now just a summary String is fine for prototype
 		data.put("physicalExamSummaryList", new ArrayList<String>());
 		data.put("prescriptions", "Enter any prescriptions here");
 		//unsure if I missed any fields, if so let me know or just add it -Chris
