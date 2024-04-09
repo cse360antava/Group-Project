6f9ec30ad3b1cc84dfbb7a732c8fc6dcda41d296
Ruze <93176347+Ruze-alt@users.noreply.github.com>
2024-04-09 05:18:38 -0700

    Updated main file

--- src/antava/Main.java
+++ src/antava/Main.java
@@ -25,9 +25,6 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
     	s = primaryStage; // initialize the singleton
     	
-    	//TODO: Doctor view: on pressing the doctor view button, go to the doctor view
-    	//TODO: Nurse view: on pressing the nurse view, go to the nurse view
-    	
     	// Add default nurse account
         Nurse defaultNurse = new Nurse(new Account("nurse", "nurse321", "nurse"));
         Main.userList.put(defaultNurse.getAccount().getUID(), defaultNurse);
@@ -44,12 +41,7 @@ public class Main extends Application {
     public static void main(String[] args) {
     	launch(args);
     	
-    	// the logo should be readily accessible to all the Views,
-    	// since all of them use it
-    	// TODO: This is a blatant hack and should be fixed later on
-    	// I couldn't get it to read the image locally,
-    	// so this fetches it from the GitHub organization's profile picture
-    	//logo = new ImageView(new Image("https://avatars.githubusercontent.com/u/163464474?s=400&u=863a0130fd7a0f9c4732e287690245896fae7e02&v=4"));
+    	
     }
     
     
